449c55aa77223fdc4fda3f08fe5996ccac68fff1
rlindsberg <roderick@karlemstrand.com>
2019-02-17 16:45:46 +0100

    Tree working

--- exercise/huffman.ex
+++ exercise/huffman.ex
@@ -14,12 +14,10 @@ defmodule Huffman do
     end
     def test do
         sample = sample()
-        tree = tree(sample)
-        table = encode_table(tree, [], "")
-        # table = decode_table(tree)
-        text = text()
-        seq = encode(text, table)
-        decode(seq, table, "")
+        |> freq
+        |> sortListByFreq
+        |> Enum.map(fn {key, value} -> {:leaf, "NULL", key, value} end) # [{:leaf, empty, " ", 78},...]
+
     end
 
     # we represent a leaf with a single character and a node as a simple tuple
@@ -28,7 +26,7 @@ defmodule Huffman do
         sample
         |> freq # returns [{"\n", 4}, {" ", 78}, {"a", 13}, {"b", 7},...]
         |> sortListByFreq # returns [{" ", 78}, {"e", 24}, {"t", 20},...]
-        |> Enum.map(fn {key, value} -> {:leaf, key, value} end) # [{:leaf, " ", 78},...]
+        |> Enum.map(fn {key, value} -> {:leaf, "NULL", key, value} end) # [{:leaf, empty, " ", 78},...]
         |> buildRightLeaningTree
         |> Enum.at(0) # remove []
 
@@ -38,12 +36,12 @@ defmodule Huffman do
 
     # result [{" ", "0"}, {"e", "10"}, {"f", "110"}, {"x", "1110"}, {"y", "11110"}]
     # makes y 11111, find disappeared z
-    def encode_table({:node, {:leaf, char1, freq1}, {:leaf, char2, freq2}, freq}, code_table, current_path) do
+    def encode_table({:node, {:leaf, empty, char1, freq1}, {:leaf, empty, char2, freq2}, freq}, code_table, current_path) do
         code_table = [code_table ++ [{char2, "#{current_path}#{0}" }] ]
         code_table = [code_table ++ [{char1, "#{current_path}#{1}" }] ]
         |> List.flatten
     end
-    def encode_table({:node, child_node, {:leaf, child_leaf_key, child_leaf_value}, freq}, code_table, current_path) do
+    def encode_table({:node, child_node, {:leaf, empty, child_leaf_key, child_leaf_value}, freq}, code_table, current_path) do
         code_table = [code_table ++ [{child_leaf_key, "#{current_path}#{0}"}] ]
         encode_table(child_node, code_table, "#{current_path}#{1}")
     end
@@ -112,23 +110,75 @@ defmodule Huffman do
         |> Enum.sort(fn({key1, value1}, {key2, value2}) -> value1 < value2 end)
     end
 
-    def buildRightLeaningTree([root_node | []]) do
-        [root_node]
-        # [left, right]
+    # def buildRightLeaningTree([root_node | []]) do
+    #     [root_node]
+    #     # [left, right]
+    # end
+    # def buildRightLeaningTree([leaf1, leaf2 | restLeaves]) do
+    #     createNode(leaf1, leaf2, restLeaves)
+    # end
+
+    # {:freq, leafOrNode, freq}
+    def buildRightLeaningTree([thingy1, thingy2 | rest_thingies]) do
+        createNode(thingy1, thingy2, rest_thingies)
+        |> rebuildTree(rest_thingies)
+        # |> buildRightLeaningTree()
+    end
+
+
+    # end of the building
+    def rebuildTree(node, []) do
+        node
+    end
+    # insert built node {_, _, _, freq} into rest_thingies.
+    def rebuildTree(new_node, rest_thingies) do
+        {_, _, _, freq} = new_node
+        index_to_insert = find_index(new_node, rest_thingies, 0)
+        [thingy1, thingy2 | rest_thingies] = List.insert_at(rest_thingies, index_to_insert, new_node) # create new tree
+        createNode(thingy1, thingy2, rest_thingies)
+        |> rebuildTree(rest_thingies)
     end
     def buildRightLeaningTree([leaf1, leaf2 | restLeaves]) do
         createNode(leaf1, leaf2, restLeaves)
     end
-    def createNode({:leaf, key1, value1}, {:leaf, key2, value2}, list) do
-        node = { :node, {:leaf, key1, value1}, {:leaf, key2, value2}, value1 + value2 } # {:node, {:leaf, "z", 1}, {:leaf, "v", 1}}
-        list = List.insert_at(list, 0, node) # insert_at(list, index, value)
-        buildRightLeaningTree(list)
+    # find node
+    def find_index({_, _, _, freq}, queue, n) do
+        # next node
+        {_, _, _, next_freq} = Enum.at(queue, n)
+        compare_freq(freq, next_freq, queue, n+1)
     end
-    def createNode({:node, left, right, value1},{:leaf, key2, value2}, list) do
-        node = {:node, {:node, left, right, value1},{:leaf, key2, value2}, value1 + value2}
-        list = List.insert_at(list, 0, node) # insert_at(list, index, value)
-        buildRightLeaningTree(list)
+    def compare_freq(freq, next_freq, queue, n) when freq < next_freq do
+        n
     end
+    def compare_freq(freq, next_freq, queue, n) do
+        find_index({"compared", "left", "right", freq}, queue, n+1)
+    end
+
+
+    # def createNode(thingy1, thingy2, []) do
+    #     {thingy1, thingy2}
+    # end
+    def createNode({:leaf, empty, key1, value1}, {:leaf, empty, key2, value2}, list) do
+        node = { :node, {:leaf, empty, key1, value1}, {:leaf, empty, key2, value2}, value1 + value2 } # {:node, {:leaf, empty, "z", 1}, {:leaf, empty, "v", 1}}
+        # list = List.insert_at(list, 0, node) # insert_at(list, index, value)
+        # buildRightLeaningTree(list)
+    end
+    def createNode({:node, left, right, value1},{:leaf, empty, key2, value2}, list) do
+        node = {:node, {:node, left, right, value1},{:leaf, empty, key2, value2}, value1 + value2}
+        # list = List.insert_at(list, 0, node) # insert_at(list, index, value)
+        # buildRightLeaningTree(list)
+    end
+    def createNode({:leaf, empty, key2, value2},{:node, left, right, value1}, list) do
+        node = {:node, {:leaf, left, right, value1},{:node, empty, key2, value2}, value1 + value2}
+    end
+    def createNode({:node, left1, right1, value1},{:node, left2, right2, value2}, list) do
+        node = {:node, {:node, left1, right1, value1},{:node, left2, right2, value2}, value1 + value2}
+    end
+
+
+
+
+
 
     # this is the public api which allows you to pass any binary representation
     def extract(str) when is_binary(str) do
