5598dc13ff684b7ae10a473204c12eb1c4efe6b7
rlindsberg <roderick@karlemstrand.com>
2019-02-19 14:13:31 +0100

    Add sklt for Env

---
+++ exercise/meta.ex
@@ -0,0 +1,21 @@
+defmodule Env do
+    # return an empty environment
+    def new() do
+
+    end
+
+    # binds variable id to the structure str and returns the environment
+    def add(id, str, env) do
+
+    end
+
+    # return either {id, str}, if the variable id was bound, or nil
+    def lookup(id, env) do
+
+    end
+    
+    # removes all bindings for variables in the list ids and returns the environment
+    def remove(ids, env) do
+
+    end
+end
