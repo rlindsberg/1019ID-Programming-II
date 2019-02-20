cab4ebf4641e2180b3415acc9ab57b5206948ad4
rlindsberg <roderick@karlemstrand.com>
2019-02-20 14:47:51 +0100

    Complete env

--- exercise/meta.ex
+++ exercise/meta.ex
@@ -1,21 +1,23 @@
 defmodule Env do
     # return an empty environment
     def new() do
-
+        []
     end
 
     # binds variable id to the structure str and returns the environment
     def add(id, str, env) do
-
+        [{id, str}] ++ env
     end
 
     # return either {id, str}, if the variable id was bound, or nil
     def lookup(id, env) do
-
+        List.keyfind(env, id, 0)
     end
-    
+
     # removes all bindings for variables in the list ids and returns the environment
     def remove(ids, env) do
-
+        List.foldr(ids, env, fn(ids, env) ->
+        List.keydelete(env, ids, 0)
+        end)
     end
 end
