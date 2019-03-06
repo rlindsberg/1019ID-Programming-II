4a5defb29c0df9b24e199d0f127376f90cb5bf30
rlindsberg <roderick@karlemstrand.com>
2019-03-06 06:54:30 +0100

    done insertion sort

---
+++ exercise/introduction/sorting.ex
@@ -0,0 +1,28 @@
+defmodule Sorting do
+    # Start by defining a function insert(element, list)
+    # that inserts the element at the right place in the list.
+    def isort(l) do isort(l, []) end
+
+    # when l is empty, list is then sorted.
+    def isort([], sortedList) do
+        sortedList
+    end
+
+    def isort([firstElementInUnsortedList | rest], sortedList) do
+        # recursively sort [firstElementInUnsortedList | rest]
+        isort(rest, insertFirstElementIntoSortedList(firstElementInUnsortedList, sortedList) )
+    end
+
+    # initial, sortedList is empty
+    def insertFirstElementIntoSortedList(element, []) do
+        [element]
+    end
+    def insertFirstElementIntoSortedList(element, [head | body]) when element > head do
+        [head | insertFirstElementIntoSortedList(element, body)]
+    end
+    # when element < head, element is then the smallest
+    def insertFirstElementIntoSortedList(element, list) do
+        [element | list]
+    end
+
+end
