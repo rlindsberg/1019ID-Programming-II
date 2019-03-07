11a04acc30fb8432c1615e92906c371b71730262
rlindsberg <roderick@karlemstrand.com>
2019-03-07 19:29:17 +0100

    done deriv for simple polynomial functions

--- exercise/introduction/derivative.ex
+++ exercise/introduction/derivative.ex
@@ -9,15 +9,26 @@ defmodule Derivative do
                 | literal()
 
     # 2x + 3 = {:add, {:mul, {:const, 2}, {:var, :x}}, {:const, 3}}
-    def deriv({:const, _}, _), do: ...
-
-    def deriv({:var, v}, v), do: ...
-
-    def deriv({:var, y}, _), do: ...
-
-    def deriv({:mul, e1, e2}, v), do: ...
-
-    def deriv({:add, e1, e2}, v), do: ...
-
+    def deriv({:const, _}, _) do
+        {:const, 0}
+    end
+    # d/dx (x) = 1
+    def deriv({:var, v}, v) do
+        {:const, 1}
+    end
+    # Partial derivative
+    def deriv({:var, y}, _) do
+        {:const, 0}
+    end
+    # derivative av multiplication
+    def deriv({:mul, e1, e2}, v) do
+        #f'x * gx + fx * g'x
+        {:add, {:mul deriv(e1, v), e2}, {:mul, e1, deriv(e2, v)}}
+    end
+    # derivative of polynom
+    def deriv({:add, e1, e2}, v) do
+        # D(fx + gx) = D(fx) + D(gx)
+        {:add, deriv(e1, v), deriv(e2, v)}
+    end
 
 end
